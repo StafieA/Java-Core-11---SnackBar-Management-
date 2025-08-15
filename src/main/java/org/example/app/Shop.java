306e883ceaf9471845f2f0cc11dbc4bb39aa73fd
Andrei Stafie <andreistafie@gmail.com>
2025-08-15 12:03:40 +0300

    Create applyRating() for an immutable object

--- src/main/java/org/example/app/Shop.java
+++ src/main/java/org/example/app/Shop.java
@@ -1,6 +1,7 @@
 package org.example.app;
 
 import org.example.data.Product;
+import org.example.data.Rating;
 
 import java.math.BigDecimal;
 
@@ -12,8 +13,11 @@ public class Shop
 {
     public static void main( String[] args )
     {
-        Product p1 = new Product(101,"tea",BigDecimal.valueOf(1.99));
+        Product p1 = new Product(101,"tea",BigDecimal.valueOf(1.99), Rating.FIVE_STAR);
+        Product p2 = p1.applyRating(Rating.THREE_STAR);
 
-        System.out.println(p1.getId()+" "+p1.getName()+" "+p1.getPrice()+" "+p1.getDiscount());
+        System.out.println(p1.getId()+" "+p1.getName()+" "+p1.getPrice()+" "+p1.getDiscount()+" "+p1.getRating().getStars());
+        System.out.println(p2.getRating().getStars());
+
     }
 }
